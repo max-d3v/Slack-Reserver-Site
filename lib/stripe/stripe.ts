e7c4d80f2f0b8bccbbdf0765ca17515cdcdd276f
max-d3v <max.maul.desenvolvedor@gmail.com>
2025-05-29 10:03:22 -0300

    correct placeholder

--- lib/stripe/stripe.ts
+++ lib/stripe/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 
 //Placeholder is only used in build time to avoid error raised.
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "placeholder");
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "api_key_placeholder");
 
 await stripe.billingPortal.configurations.create({
   features: {
