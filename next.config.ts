02afe8c3b894738598977fa3574740eceb061d8c
max-d3v <max.maul.desenvolvedor@gmail.com>
2025-06-04 14:30:30 -0300

    added gmail secret env to build time

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
-  env: {
-    SITE_URL: process.env.SITE_URL,
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-  },
 };
 
 export default nextConfig;
