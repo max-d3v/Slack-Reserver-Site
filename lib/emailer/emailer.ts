28508be88827b8201785afdfc6e009a2325e8484
max-d3v <max.maul.desenvolvedor@gmail.com>
2025-06-05 08:39:01 -0300

    changed emailer for ssl

--- lib/emailer/emailer.ts
+++ lib/emailer/emailer.ts
@@ -3,7 +3,9 @@ import { CONSTANTS } from '../constants';
 
 
 export const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,           // SSL port
+    secure: true,        // true for SSL
     auth: {
         user: CONSTANTS.CONTACT_EMAIL,
         pass: process.env.GMAIL_APP_PASSWORD
