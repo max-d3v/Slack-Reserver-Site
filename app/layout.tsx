3886405d134d07605a9958df4b241c325c2f91af
max-d3v <max.maul.desenvolvedor@gmail.com>
2025-06-16 09:59:25 -0300

    removed development banner for now

--- app/layout.tsx
+++ app/layout.tsx
@@ -144,7 +144,7 @@ export default async function RootLayout({
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
         <SessionProvider session={session}>
-        <DevelopmentBanner />
+        
           <Navbar />
           {children}
           <Footer />
